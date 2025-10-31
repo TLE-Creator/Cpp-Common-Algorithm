518dab8bd071d135b2b1827b6e69d37808b4d410
TLE-Creator <love810701@163.com>
2025-10-31 14:30:13 +0800

    Add: Add Edge structure definition for Kruskal's algorithm

--- include/Edge.h
+++ include/Edge.h
@@ -1,6 +1,17 @@
 #ifndef EDGE_H
 #define EDGE_H
 
+/*
+* Description：
+
+* The graph structure on which Kruskal's minimum spanning tree depends
+
+* @code
+
+* Edge graph;
+
+* @endcode
+*/
 struct Edge{
     int u,v,weight;
 
