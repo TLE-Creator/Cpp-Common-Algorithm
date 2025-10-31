073ccbd8af9587e517327df5e3054fc00e378828
TLE-Creator <love810701@163.com>
2025-10-31 14:35:54 +0800

    Add: Add the description of The High Precision Algorithm.

--- include/High Precision.h
+++ include/High Precision.h
@@ -1,6 +1,22 @@
 #ifndef HIGH_PRECESION_H
 #define HIGH_PRECESION_H
 
+/*
+* Description:
+
+* The High Precision Algorithm.
+
+* @code
+
+* string a,b;
+
+* cin>>a>>b;
+
+* string sum=addition(a,b);
+
+* cout<<sum;
+*/
+
 #include <string>
 
 std::string addition(const std::string& a,const std::string& b){}
