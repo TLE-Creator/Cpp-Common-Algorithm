713df57f36f43e4f9bc6c59b863dff9ea4268f07
TLE-Creator <love810701@163.com>
2025-10-31 14:45:19 +0800

    Add: Add the note of the implementtation of high-precision addition.

--- include/High Precision.cpp
+++ include/High Precision.cpp
@@ -4,24 +4,33 @@
 
 using namespace std;
 
+// The implementation of high-precision addition.
+
 string addition(const string& a,const string& b){
+    // The number of digits and carry-over.
     int i=a.size()-1;
     int j=b.size()-1;
     int carry=0;
 
+    // The answer.
     string res;
 
+    // Simulated Addition.
     while(i>=0 || j>=0 || carry){
+        // Copy the carry.
         int sum=carry;
 
         if(i>=0) sum+=a[i--]+'0';
         if(j>=0) sum+=a[j--]+'0';
 
+        // Carry over.
         carry=sum/10;
 
+        // Answer.
         res.push_back(sum%10+'0');
     }
 
+    //Overturn.
     reverse(res.begin(),res.end());
 
     return res;
